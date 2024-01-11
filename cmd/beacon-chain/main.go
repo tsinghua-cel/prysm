aa43218af4be2c837eca4936d98553fb4848530c
sager <xueqian1991@gmail.com>
2024-01-11 10:21:12 +0800

    add attacker and cmd flag.

--- cmd/beacon-chain/main.go
+++ cmd/beacon-chain/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/prysmaticlabs/prysm/v4/attacker"
 	"os"
 	"path/filepath"
 	runtimeDebug "runtime/debug"
@@ -90,6 +91,7 @@ var appFlags = []cli.Flag{
 	cmd.P2PMaxPeers,
 	cmd.P2PPrivKey,
 	cmd.P2PPrivHex,
+	cmd.Attacker,
 	cmd.P2PStaticID,
 	cmd.P2PMetadata,
 	cmd.P2PAllowList,
@@ -286,6 +288,7 @@ func startNode(ctx *cli.Context) error {
 			opts = append(opts, ofo)
 		}
 	}
+	attacker.InitAttacker(ctx.String(cmd.Attacker.Name))
 
 	beacon, err := node.New(ctx, opts...)
 	if err != nil {
