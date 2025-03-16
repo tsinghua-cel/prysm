e7952c3622d89771e67fa8052c4f9dd8a0af6461
sager <xueqian1991@gmail.com>
2025-03-16 10:40:28 +0800

    add dockerfile

---
+++ docker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+docker build --no-cache -t tscel/bf.beacon:v5.2.0 -f Dockerfile.beacon .
+docker build --no-cache -t tscel/bf.validator:v5.2.0 -f Dockerfile.validator .
+
+docker push tscel/bf.beacon:v5.2.0
+docker push tscel/bf.validator:v5.2.0
