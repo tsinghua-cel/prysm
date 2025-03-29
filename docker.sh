302ac5016e694a226857ef78a5bbb8eed55570c2
sager <xueqian1991@gmail.com>
2025-03-29 09:35:59 +0800

    add docker file for prysmctl

--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 docker build --no-cache -t tscel/bf.beacon:v5.2.0 -f Dockerfile.beacon .
 docker build --no-cache -t tscel/bf.validator:v5.2.0 -f Dockerfile.validator .
+docker build --no-cache -t tscel/prysmctl:v5.2.0 -f Dockerfile.prysmctl .
 
 docker push tscel/bf.beacon:v5.2.0
 docker push tscel/bf.validator:v5.2.0
+docker push tscel/prysmctl:v5.2.0
