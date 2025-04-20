a383ee267aa5ef78002982eea7b9e5ea9a181243
sager <xueqian1991@gmail.com>
2025-04-20 12:48:03 +0800

    add dockerfile for build docker images

--- docker.sh
+++ docker.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
-docker build --no-cache -t tscel/bf.beacon:v5.1.2 -f Dockerfile.beacon .
-docker build --no-cache -t tscel/bf.validator:v5.1.2 -f Dockerfile.validator .
+docker build --no-cache -t tscel/bf.prysm:v5.1.2 -f Dockerfile.prysm .
 docker build --no-cache -t tscel/prysmctl:v5.1.2 -f Dockerfile.prysmctl .
 
-docker push tscel/bf.beacon:v5.1.2
-docker push tscel/bf.validator:v5.1.2
-docker push tscel/prysmctl:v5.1.2
+#docker push tscel/bf.prysm:v5.1.2
+#docker push tscel/prysmctl:v5.1.2
