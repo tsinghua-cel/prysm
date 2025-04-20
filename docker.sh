c94b60cea925b06ffa81ee59db738a064bb06942
sager <xueqian1991@gmail.com>
2025-04-20 11:08:51 +0800

    update docker file

--- docker.sh
+++ docker.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
-docker build --no-cache -t tscel/bf.beacon:v5.2.0 -f Dockerfile.beacon .
-docker build --no-cache -t tscel/bf.validator:v5.2.0 -f Dockerfile.validator .
-docker build --no-cache -t tscel/prysmctl:v5.2.0 -f Dockerfile.prysmctl .
+#docker build --no-cache -t tscel/bf.beacon:v5.2.0 -f Dockerfile.beacon .
+#docker build --no-cache -t tscel/bf.validator:v5.2.0 -f Dockerfile.validator .
+#docker push tscel/bf.beacon:v5.2.0
+#docker push tscel/bf.validator:v5.2.0
 
-docker push tscel/bf.beacon:v5.2.0
-docker push tscel/bf.validator:v5.2.0
+docker build --no-cache -t tscel/bf.prysm:v5.2.0 -f Dockerfile.prysm .
+docker build --no-cache -t tscel/prysmctl:v5.2.0 -f Dockerfile.prysmctl .
+docker push tscel/bf.prysm:v5.2.0
 docker push tscel/prysmctl:v5.2.0
