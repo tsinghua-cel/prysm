a40fe46abfbdd2292348b19e7932ae63ea1ef5eb
sager <xueqian1991@gmail.com>
2025-04-20 13:47:48 +0800

    add docker file for prysm

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-docker build --no-cache -t tscel/bf.prysm:v5.1.2 -f Dockerfile.prysm .
-docker build --no-cache -t tscel/prysmctl:v5.1.2 -f Dockerfile.prysmctl .
+docker build --no-cache -t tscel/bf.prysm:v5.3.3 -f Dockerfile.prysm .
+docker build --no-cache -t tscel/prysmctl:v5.3.3 -f Dockerfile.prysmctl .
 
-#docker push tscel/bf.prysm:v5.1.2
-#docker push tscel/prysmctl:v5.1.2
+#docker push tscel/bf.prysm:v5.3.3
+#docker push tscel/prysmctl:v5.3.3
