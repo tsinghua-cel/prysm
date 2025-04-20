e4f3d3a8887db2105d184c02c6da1b0c163a5833
sager <xueqian1991@gmail.com>
2025-04-20 18:17:01 +0800

    update docker version

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-docker build --no-cache -t tscel/bf.prysm:v4.2.1 -f Dockerfile.prysm .
-docker build --no-cache -t tscel/prysmctl:v4.2.1 -f Dockerfile.prysmctl .
+docker build --no-cache -t tscel/bf.prysm:v4.0.5 -f Dockerfile.prysm .
+docker build --no-cache -t tscel/prysmctl:v4.0.5 -f Dockerfile.prysmctl .
 
 #docker push tscel/bf.prysm:v4.2.1
 #docker push tscel/prysmctl:v4.2.1
