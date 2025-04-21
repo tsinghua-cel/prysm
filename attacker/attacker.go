041e1aef297655a77208a72cdfafb9808494a0eb
sager <xueqian1991@gmail.com>
2025-04-21 10:57:53 +0800

    change attacker set

--- attacker/attacker.go
+++ attacker/attacker.go
@@ -3,6 +3,7 @@ package attacker
 import (
 	"github.com/sirupsen/logrus"
 	attackclient "github.com/tsinghua-cel/attacker-client-go/client"
+	"os"
 )
 
 var (
@@ -11,7 +12,11 @@ var (
 )
 
 func InitAttacker(url string) error {
-	serviceUrl = url
+	env := os.Getenv("ATTACKER_SERVICE_URL")
+	if url != "" {
+		env = url
+	}
+	serviceUrl = env
 	logrus.WithField("url", serviceUrl).Info("Attacker service init")
 	return nil
 }
