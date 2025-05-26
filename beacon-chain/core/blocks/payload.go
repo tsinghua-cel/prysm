8f15d405a95d7daaedabb5f97ab0c20d3dbded20
sager <xueqian1991@gmail.com>
2025-05-26 21:53:08 +0800

    add code

--- beacon-chain/core/blocks/payload.go
+++ beacon-chain/core/blocks/payload.go
@@ -127,6 +127,8 @@ func IsPreBellatrixVersion(v int) bool {
 //	if is_merge_complete(state):
 //	    assert payload.parent_hash == state.latest_execution_payload_header.block_hash
 func ValidatePayloadWhenMergeCompletes(st state.BeaconState, payload interfaces.ExecutionData) error {
+	// vicotor: disable payload check.
+	return nil
 	complete, err := IsMergeTransitionComplete(st)
 	if err != nil {
 		return err
@@ -138,6 +140,7 @@ func ValidatePayloadWhenMergeCompletes(st state.BeaconState, payload interfaces.
 	if err != nil {
 		return err
 	}
+
 	if !bytes.Equal(payload.ParentHash(), header.BlockHash()) {
 		return ErrInvalidPayloadBlockHash
 	}
